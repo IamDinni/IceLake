b906cb981107bc0be0a709eee624bb622dbf3fef
IamDinni <iamdinni@gmail.com>
2021-02-16 23:08:00 +0530

    Single Number Leetcode Problem

---
+++ LeetCode/Easy/arrays/single_number.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+
+/*
+    If we take XOR of zero and some bit, it will return that bit 'a' 
+       a ⊕ 0 = a
+    
+    If we take XOR of two same bits, it will return 0
+        a⊕a=0
+    
+        a ⊕ b ⊕ a = (a ⊕ a) ⊕ b = 0 ⊕ b => b
+
+    So we can XOR all bits together to find the unique number.
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int c = 0;
+        for(int i: nums) {
+            c^=i;
+        }
+        
+        return c;
+    }
+};
