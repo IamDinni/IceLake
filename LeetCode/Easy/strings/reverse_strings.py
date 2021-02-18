ac31827182d4ffcc923c0bbfe52573da7d8993c5
IamDinni <iamdinni@gmail.com>
2021-02-19 00:07:04 +0530

    Leetcode strings problem

---
+++ LeetCode/Easy/strings/reverse_strings.py
@@ -0,0 +1,11 @@
+'''
+    Time: O(N) to swap N/2 element
+    Space: O(1)
+'''
+
+class Solution:
+    def reverseString(self, s):
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
