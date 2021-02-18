3e0f1b96e62393fc306d8ef0be880042490ee0b9
IamDinni <iamdinni@gmail.com>
2021-02-18 19:04:26 +0530

    Valid sudoku problem

---
+++ LeetCode/Easy/arrays/valid_sudoku.py
@@ -0,0 +1,32 @@
+'''
+    Time: O(1) since all we do here is just one iteration over the board with 81 cells.
+    Space: O(1)
+'''
+
+class Solution:
+    def isValidSudoku(self, board):
+        """
+        Board is List[List[str]] -> returns bool
+        """
+        # init data
+        rows = [{} for i in range(9)]
+        columns = [{} for i in range(9)]
+        boxes = [{} for i in range(9)]
+
+        # validate a board
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != '.':
+                    num = int(num)
+                    box_index = (i // 3 ) * 3 + j // 3
+                    
+                    # keep the current cell value
+                    rows[i][num] = rows[i].get(num, 0) + 1
+                    columns[j][num] = columns[j].get(num, 0) + 1
+                    boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+                    
+                    # check if this value has been already seen before
+                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
+                        return False         
+        return True
