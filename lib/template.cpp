c448b438692b718af6ca68ebd2c0a43d0a2dbd68
IamDinni <iamdinni@gmail.com>
2021-03-05 00:41:12 +0530

    Changes in CP Template

--- lib/template.cpp
+++ lib/template.cpp
@@ -2,40 +2,84 @@
 #include <string>
 #include <cstdio>
 #include <cmath>
-
-typedef long long ll;
-
-#define f first
-#define s second
-#define rep(i, n) for (i = 0; i < n; ++i)
-#define REP(i, k, n) for (i = k; i <= n; ++i)
-#define REPR(i, k, n) for (i = k; i >= n; --i)
-
-const ll MOD = 1e9 + 7;
+#include <vector>
+#include <pair>
 
 using namespace std;
 
-void setIO(string str) { 
-	freopen((str + ".in").c_str(), "r", stdin);
-	freopen((str + ".out").c_str(), "w", stdout);
+using str = string; // python style
+using ll = long long;
+using ld = long double;
+using db = double;
+
+using pi = pair<int,int>;
+using pl = pair<ll, ll>;
+using pd = pair<db, db>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+// Pairs
+#define f first
+#define s second
+#define mp make_pair
+
+// Loops
+#define FOR(i, First, Last) for(int i = (First); i < (Last); ++i)
+#define FOR(i, Last) FOR(i, 0, Last)
+#define ROF(i, Last, First) for(int i = (Last) - 1; i >= (First); --i)
+#define ROF(i, Last) ROF(i, 0, Last)
+#define loop(item, items) for(auto& item: items)
+
+// Vectors
+#define pb push_back
+#define pf push_front
+#define ins insert
+#define lb lower_bound
+#define ub upper_bound
+#define eb emplace_back
+#define rsz resize
+#define rb rbegin
+#define re rend
+#define bk back()
+#define ft front()
+#define all(x) begin(x), end(x)
+#define sor(x) sort(all(x))
+#define sz(x) (int)(x).size()
+#define rall(x) (x).rb(), (x).re()
+
+// Contants
+#define int MOD = 1e9 + 7;
+
+void setIO(str s = "") { 
+    freopen((str + ".in").c_str(), "r", stdin);
+    freopen((str + ".out").c_str(), "w", stdout);
+}
+
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    setIO("name")
+    fastIO();
+    setIO("filename");
 
     int T;
     int N;
     cin >> T;
-    while (T--)
-    {
+    
+    FOR(i, 0, T) {
         cin >> N;
         int arr[N];
-        for (int &i : arr)
-            cin >> i;
+        loop(i, arr) cin >> i;
     }
 
     return 0;
