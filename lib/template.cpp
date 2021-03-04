10c60caa3a5f21f352a031ced20069ffca508b9e
IamDinni <iamdinni@gmail.com>
2021-03-04 13:43:07 +0530

    CPP Template changes

--- lib/template.cpp
+++ lib/template.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <string>
+#include <cstdio>
 #include <cmath>
 
+typedef long long ll;
+
+#define f first
+#define s second
 #define rep(i, n) for (i = 0; i < n; ++i)
 #define REP(i, k, n) for (i = k; i <= n; ++i)
 #define REPR(i, k, n) for (i = k; i >= n; --i)
 
+const ll MOD = 1e9 + 7;
+
 using namespace std;
+
+void setIO(string str) { 
+	freopen((str + ".in").c_str(), "r", stdin);
+	freopen((str + ".out").c_str(), "w", stdout);
+}
 
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    setIO("name")
+
     int T;
     int N;
     cin >> T;
